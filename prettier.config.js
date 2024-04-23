e6bb3faeb5da9321ef564e4f792da35e7ff3bf73
Prisca <prisca.castanier@gmail.com>
2024-04-24 01:11:18 +0200

    misc: update prettier config and gitignore

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+	plugins: ['prettier-plugin-tailwindcss'],
+	singleQuote: true,
+	semi: true,
+	tabWidth: 2,
+	useTabs: true,
+	printWidth: 80,
+	trailingComma: 'all',
 };
 
 export default config;
