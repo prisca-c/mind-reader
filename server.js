2cd95adcb596c3de8fe49e8805a4c6193191c86c
Prisca <prisca.castanier@gmail.com>
2024-04-25 00:26:25 +0200

    chore(env): add port and hostname to env

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ import next from 'next';
 import { Server } from 'socket.io';
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = 3000;
-// when using middleware `hostname` and `port` must be provided below
+const hostname = process.env.HOSTNAME;
+const port = parseInt(process.env.PORT ?? '3000');
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
@@ -29,6 +28,8 @@ void app.prepare().then(() => {
 			process.exit(1);
 		})
 		.listen(port, () => {
+			const hostname = process.env.HOSTNAME;
+			const port = parseInt(process.env.PORT ?? '3000');
 			console.log(`> Ready on http://${hostname}:${port}`);
 		});
 });
