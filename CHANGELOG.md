01c75a4cf181abeaeae6ec41690695390d922c55
Prisca <prisca.castanier@gmail.com>
2024-05-01 14:50:19 +0200

    Switch project to AdonisJS

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,4 +6,3 @@
 
 ## [0.1.1] - 2024-04-25
 ### Added
-- Add Socket.io to the project.
--- src/app/[locale]/game/training/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TrainingPage() {
-	return (
-		<div>
-			<h1>Training Page</h1>
-		</div>
-	);
-}
