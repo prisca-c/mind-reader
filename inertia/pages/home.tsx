2183b8133aabdfa1385f8d5cc07e0f00fd7c464c
Prisca <prisca.castanier@gmail.com>
2024-05-07 22:54:21 +0200

    feat(i18n): setup i18n on home page

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,18 +1,20 @@
 import { Head, router } from '@inertiajs/react'
+import { useTranslation } from 'react-i18next'
 
 export default function Home() {
+  const { t } = useTranslation()
   return (
     <div className={'flex justify-center items-center h-screen w-screen'}>
       <Head title="Homepage" />
 
       <div>
-        <h1 className={'text-4xl font-bold'}>Mind Reader</h1>
-        <p className={'text-lg'}>Welcome to the Mind Reader game</p>
+        <h1 className={'text-4xl font-bold'}>{t('home.title')}</h1>
+        <p className={'text-lg'}>{t('home.description')}</p>
         <button
           className={'border-2 border-gray-500 hover:bg-gray-500 hover:text-white p-2 rounded-md'}
           onClick={() => router.visit('/login')}
         >
-          Start Game
+          {t('home.buttons.login')}
         </button>
       </div>
     </div>
