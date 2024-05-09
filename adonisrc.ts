6579915a7c81b8c2aad1bd5869a3d57ec5b7e05a
Prisca <prisca.castanier@gmail.com>
2024-05-09 05:00:43 +0200

    chore: add transmit and setup base on api and client

--- adonisrc.ts
+++ adonisrc.ts
@@ -40,7 +40,8 @@ export default defineConfig({
     () => import('@adonisjs/inertia/inertia_provider'),
     () => import('@adonisjs/i18n/i18n_provider'),
     () => import('@adonisjs/redis/redis_provider'),
-    () => import('@adonisjs/ally/ally_provider')
+    () => import('@adonisjs/ally/ally_provider'),
+    () => import('@adonisjs/transmit/transmit_provider')
   ],
 
   /*
