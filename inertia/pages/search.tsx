8da67cc7c7c8fd4fe93af84ca0efc3e38f4f0b35
Prisca <prisca.castanier@gmail.com>
2024-05-14 23:03:31 +0200

    feat(game): search page now listen to queue count and user channel

--- inertia/pages/search.tsx
+++ inertia/pages/search.tsx
@@ -1,6 +1,6 @@
 import { useTransmit } from '~/hooks/use_transmit'
 import User from '#models/user'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Api } from '~/services/api'
 
 type Props = {
@@ -8,21 +8,34 @@ type Props = {
 }
 
 export default function Search({ user }: Props) {
-  const { subscription } = useTransmit({ url: `/game/user/${user.id}` })
-  subscription?.onMessage((message) => {
-    console.log(message)
-  })
+  const [queueCount, setQueueCount] = useState(0)
+  const { subscription: userListener } = useTransmit({ url: `game/user/${user.id}` })
+  const { subscription: queueListener } = useTransmit({ url: 'game/search' })
 
   const registerToQueue = async () => {
-    await new Api().post('/game/search')
+    const response: { message: string; queueCount: number } = await new Api().post('/game/search')
+    setQueueCount(response.queueCount)
   }
 
   useEffect(() => {
     registerToQueue()
   }, [])
+
+  userListener?.create()
+  queueListener?.create()
+
+  userListener?.onMessage((message) => {
+    console.log(message)
+  })
+
+  queueListener?.onMessage((message: { queueCount: number }) => {
+    setQueueCount(message.queueCount)
+  })
+
   return (
     <div>
       <h1>Search</h1>
+      <p>Queue count: {queueCount}</p>
     </div>
   )
 }
