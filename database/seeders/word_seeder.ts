6b0d925680aeef2dea607a7c6160606c5824c838
Prisca <prisca.castanier@gmail.com>
2024-05-18 16:48:19 +0200

    feat(seed): add a seeder to generate words

---
+++ database/seeders/word_seeder.ts
@@ -0,0 +1,54 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Word from '#models/word'
+
+export default class extends BaseSeeder {
+  async run() {
+    const wordsEn: Partial<Word>[] = [
+      {
+        name: 'apple',
+        language: 'en',
+      },
+      {
+        name: 'banana',
+        language: 'en',
+      },
+      {
+        name: 'cherry',
+        language: 'en',
+      },
+      {
+        name: 'date',
+        language: 'en',
+      },
+      {
+        name: 'elderberry',
+        language: 'en',
+      },
+    ]
+
+    const wordsFr: Partial<Word>[] = [
+      {
+        name: 'abricot',
+        language: 'fr',
+      },
+      {
+        name: 'banane',
+        language: 'fr',
+      },
+      {
+        name: 'cerise',
+        language: 'fr',
+      },
+      {
+        name: 'datte',
+        language: 'fr',
+      },
+      {
+        name: 'framboise',
+        language: 'fr',
+      },
+    ]
+
+    await Word.createMany([...wordsEn, ...wordsFr])
+  }
+}
