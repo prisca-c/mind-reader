911f481734db67ec4ba8d0e87214fc00ece3379f
Prisca <prisca.castanier@gmail.com>
2024-05-18 19:23:35 +0200

    refactor(features): add controllers folder inside features

--- start/routes.ts
+++ start/routes.ts
@@ -14,11 +14,12 @@ import { middleware } from '#start/kernel'
 const AuthController = () => import('#controllers/auth_controller')
 const PagesController = () => import('#controllers/pages_controller')
 const ChatController = () => import('#controllers/chat_controller')
-const GameAnswerController = () => import('#features/game_session/game_answer_controller')
+const GameAnswerController = () =>
+  import('#features/game_session/controllers/game_answer_controller')
 const SearchMatchmakingController = () =>
-  import('#features/matchmaking/search_matchmaking_controller')
+  import('#features/matchmaking/controllers/search_matchmaking_controller')
 const AcceptMatchmakingController = () =>
-  import('#features/matchmaking/accept_matchmaking_controller')
+  import('#features/matchmaking/controllers/accept_matchmaking_controller')
 // endregion
 
 router.get('/', [PagesController, 'home']).as('home')
