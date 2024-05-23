bbd61005a4ae7d3fb414741496be2ec2969ee844
Prisca <prisca.castanier@gmail.com>
2024-05-23 16:23:08 +0200

    refactor(layout): add Header component

---
+++ inertia/features/layout/header.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import i18n from '~/features/i18n/i18n'
+
+export const Header = () => {
+  const [language, setLanguage] = React.useState<string>('en')
+
+  React.useEffect(() => {
+    const locale = document.cookie.split(';').find((cookie) => cookie.includes('locale'))
+    if (locale) {
+      const cookieLang = locale.split('=')[1]
+      i18n.changeLanguage(cookieLang)
+      setLanguage(cookieLang)
+    } else {
+      i18n.changeLanguage('en')
+      document.cookie = 'locale=en;path=/'
+      setLanguage('en')
+    }
+  }, [])
+
+  const changeLanguage = async (lang: string) => {
+    await i18n.changeLanguage(lang)
+    document.cookie = `locale=${lang};path=/`
+    setLanguage(lang)
+  }
+  return (
+    <div className={'absolute top-4 right-4 flex items-center'}>
+      {language === 'en' ? (
+        <img
+          src={'/images/flags/united-states-of-america-flag.svg'}
+          alt={'United States flag'}
+          className={'h-[10px] w-auto'}
+        />
+      ) : (
+        <img
+          src={'/images/flags/france-flag.svg'}
+          alt={'Drapeau français'}
+          className={'h-[10px] w-auto'}
+        />
+      )}
+      <select className={'p-2'} onChange={(e) => changeLanguage(e.target.value)} value={language}>
+        <option value={'en'}>🇺🇸</option>
+        <option value={'fr'}>🇫🇷</option>
+      </select>
+    </div>
+  )
+}
--- inertia/features/layout/layout.tsx
+++ inertia/features/layout/layout.tsx
@@ -1,55 +1,12 @@
 import React from 'react'
-import i18n from '~/features/i18n/i18n'
 import { Footer } from '~/features/layout/footer'
+import { Header } from '~/features/layout/header'
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
-  const [language, setLanguage] = React.useState<string>('en')
-
-  React.useEffect(() => {
-    const locale = document.cookie.split(';').find((cookie) => cookie.includes('locale'))
-    if (locale) {
-      const cookieLang = locale.split('=')[1]
-      i18n.changeLanguage(cookieLang)
-      setLanguage(cookieLang)
-    } else {
-      i18n.changeLanguage('en')
-      document.cookie = 'locale=en;path=/'
-      setLanguage('en')
-    }
-  }, [])
-
-  const changeLanguage = async (lang: string) => {
-    await i18n.changeLanguage(lang)
-    document.cookie = `locale=${lang};path=/`
-    setLanguage(lang)
-  }
-
   return (
     <div>
       <div className={'relative flex justify-center items-center h-screen w-auto'}>
-        <div className={'absolute top-4 right-4 flex items-center'}>
-          {language === 'en' ? (
-            <img
-              src={'/images/flags/united-states-of-america-flag.svg'}
-              alt={'United States flag'}
-              className={'h-[10px] w-auto'}
-            />
-          ) : (
-            <img
-              src={'/images/flags/france-flag.svg'}
-              alt={'Drapeau français'}
-              className={'h-[10px] w-auto'}
-            />
-          )}
-          <select
-            className={'p-2'}
-            onChange={(e) => changeLanguage(e.target.value)}
-            value={language}
-          >
-            <option value={'en'}>🇺🇸</option>
-            <option value={'fr'}>🇫🇷</option>
-          </select>
-        </div>
+        <Header />
         {children}
       </div>
 
