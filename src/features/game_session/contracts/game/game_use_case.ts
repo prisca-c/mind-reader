bbf283f3defc0b9d1af2eef3cd0a4d4b4d1553ef
Prisca <prisca.castanier@gmail.com>
2024-08-03 16:49:29 +0200

    feat(game): add game rules to use cases

--- src/features/game_session/contracts/game/game_use_case.ts
+++ src/features/game_session/contracts/game/game_use_case.ts
@@ -2,13 +2,16 @@ import type { HttpContext } from '@adonisjs/core/http'
 import type { GameSession, GameSessionId } from '#features/game_session/types/game_session'
 import { inject } from '@adonisjs/core'
 import { GamePort } from '#features/game_session/contracts/game/game_port'
+import { GameRules } from '#features/game_session/contracts/game_rules/game_rules'
 
 @inject()
 export class GameUseCase {
   private gamePort: GamePort
+  private gameRules: GameRules
 
-  constructor(gamePort: GamePort) {
+  constructor(gamePort: GamePort, gameRules: GameRules) {
     this.gamePort = gamePort
+    this.gameRules = gameRules
   }
 
   async handle(ctx: HttpContext): Promise<void> {
@@ -24,78 +27,27 @@ export class GameUseCase {
       return response.notFound()
     }
 
-    const { player1, player2, hintGiver, word, wordsList } = session
-    const answer: string = request.input('answer')
-
-    if (player1.id !== user.id && player2.id !== user.id) {
+    if (this.gameRules.isGuesser(user.id, session.player1.id, session.player2.id)) {
       return response.unauthorized()
     }
 
-    if (!hintGiver || !word || !player1 || !player2) {
-      return response.badRequest({ message: 'Invalid game session' })
+    const answer: string = request.input('answer')
+    if (!this.gameRules.validWord(session, user.id, answer)) {
+      await this.gamePort.broadcastError(session)
+      return response.ok({ message: 'Error' })
     }
 
-    if (hintGiver === user.id) {
-      if (word.startsWith(answer.slice(0, 3)) || word.endsWith(answer.slice(-3))) {
-        await this.gamePort.broadcastError(session)
-        return response.ok({ message: 'Error' })
-      }
+    const isHintGiver = session.hintGiver === user.id
+    const isCorrect = this.gameRules.validateAnswer(session, answer, user.id)
+    const updatedSession = isHintGiver
+      ? this.gameRules.updateSessionForHintGiver(session, answer)
+      : this.gameRules.updateSessionForGuesser(session, answer, isCorrect)
 
-      const updatedSession: GameSession = {
-        ...session,
-        turn: hintGiver !== player1.id ? player1.id : player2.id,
-        wordsList: {
-          hintGiver: [...wordsList.hintGiver, answer],
-          guesser: [...wordsList.guesser],
-        },
-      }
-      await this.gamePort.updateSession(updatedSession)
-      await this.gamePort.broadcastAnswer(updatedSession, false)
-      return response.ok({ message: 'Success' })
-    }
-
-    if (hintGiver !== user.id) {
-      if (answer.toLowerCase() === word.toLowerCase()) {
-        const updatedSession: GameSession = {
-          ...session,
-          turn: null,
-          guessed: true,
-          wordsList: {
-            hintGiver: [...wordsList.hintGiver],
-            guesser: [...wordsList.guesser, answer],
-          },
-        }
-        await this.#handleSessionUpdate(updatedSession, true, true)
-        return response.ok({ message: 'Success' })
-      }
-
-      let updatedSession: GameSession = {
-        ...session,
-        turn: hintGiver,
-        wordsList: {
-          hintGiver: [...wordsList.hintGiver],
-          guesser: [...wordsList.guesser, answer],
-        },
-      }
-
-      if (updatedSession.wordsList.guesser.length === 5) {
-        updatedSession = {
-          ...updatedSession,
-          turn: null,
-          guessed: false,
-        }
-        await this.#handleSessionUpdate(updatedSession, true)
-        return response.ok({ message: 'Success' })
-      }
-
-      await this.#handleSessionUpdate(updatedSession, false)
-      return response.ok({ message: 'Answer sent' })
-    }
-
-    return response.badRequest({ message: 'Invalid user' })
+    await this.handleSessionUpdate(updatedSession, isCorrect, isCorrect)
+    return response.ok({ message: 'Success' })
   }
 
-  async #handleSessionUpdate(
+  private async handleSessionUpdate(
     updatedSession: GameSession,
     saveGame: boolean,
     win = false
