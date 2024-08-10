50d8a4becd32c9b0820ba874189d78bfb96b7626
Prisca <prisca.castanier@gmail.com>
2024-08-10 04:35:06 +0200

    fix(auth): avoid lastSessionId check in test env bc of session id not being settable

--- src/core/middleware/auth_middleware.ts
+++ src/core/middleware/auth_middleware.ts
@@ -1,6 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 import type { Authenticators } from '@adonisjs/auth/types'
+import env from '#start/env'
 
 /**
  * Auth middleware is used authenticate HTTP requests and deny
@@ -20,8 +21,9 @@ export default class AuthMiddleware {
     const isAuthenticated = await ctx.auth.authenticateUsing(options.guards)
     const user = ctx.auth.user
     const currentSessionId = ctx.session.sessionId
+    const isTest = env.get('NODE_ENV') === 'test'
 
-    if (isAuthenticated && user?.lastSessionId !== currentSessionId) {
+    if (isAuthenticated && user?.lastSessionId !== currentSessionId && !isTest) {
       await ctx.auth.use('web').logout()
       ctx.response.redirect(this.redirectTo)
     } else if (!isAuthenticated) {
