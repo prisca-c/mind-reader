94302036fa4da1141f6ebd19d11b2f75a4cc5bad
Prisca <prisca.castanier@gmail.com>
2024-08-13 10:21:57 +0200

    fix: fix test gameRules

--- tests/unit/game_rules/update_session.spec.ts
+++ tests/unit/game_rules/update_session.spec.ts
@@ -1,10 +1,11 @@
 import { test } from '@japa/runner'
 import { GameRules } from '#features/game_session/contracts/game_rules/game_rules'
 import { player1, player2, session } from '#tests/utils/session_data'
+import { GameSession } from '#features/game_session/types/game_session'
 
 test.group('GameRules - updateSessionForHintGiver & updateSessionForGuesser', () => {
   const gameRules = new GameRules()
-  const sessionCopy = { ...session, hintGiver: player1.id }
+  const sessionCopy: GameSession = { ...session, hintGiver: player1.id, word: 'apple' }
 
   test('should update session after hint giver gives a word', ({ assert }) => {
     const result = gameRules.updateSessionForHintGiver(sessionCopy, 'apple')
@@ -15,7 +16,7 @@ test.group('GameRules - updateSessionForHintGiver & updateSessionForGuesser', ()
   })
 
   test('should update session after guesser guesses a word which does not match', ({ assert }) => {
-    const result = gameRules.updateSessionForGuesser(sessionCopy, 'fruit', false)
+    const result = gameRules.updateSessionForGuesser(sessionCopy, 'fruit', false, false)
     Object.assign(sessionCopy, result)
     assert.equal(sessionCopy.turn, player1.id)
     assert.equal(sessionCopy.guessed, false)
@@ -24,7 +25,7 @@ test.group('GameRules - updateSessionForHintGiver & updateSessionForGuesser', ()
   })
 
   test('should update session after guesser guesses a word and it matches', ({ assert }) => {
-    const result = gameRules.updateSessionForGuesser(sessionCopy, 'apple', true)
+    const result = gameRules.updateSessionForGuesser(sessionCopy, 'apple', true, true)
     Object.assign(sessionCopy, result)
     assert.equal(sessionCopy.turn, null)
     assert.equal(sessionCopy.guessed, true)
