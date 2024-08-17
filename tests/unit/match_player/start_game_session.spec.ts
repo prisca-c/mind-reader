5d2ab7c029c4b3329f3585add947d40ec8213259
Prisca <prisca.castanier@gmail.com>
2024-08-17 14:00:15 +0200

    test(match): add test to handle start_game_session

---
+++ tests/unit/match_player/start_game_session.spec.ts
@@ -0,0 +1,70 @@
+import { test } from '@japa/runner'
+import { session } from '#tests/utils/session_data'
+import { MatchPlayerJob } from '#features/matchmaking/jobs/match_player_job'
+import { CacheService } from '#services/cache/cache_service'
+import { randomUUID } from 'node:crypto'
+import type { GameSessionId } from '#features/game_session/types/game_session'
+import { EventStreamService } from '#services/event_stream/event_stream_service'
+import Word from '#models/word'
+
+test.group('MatchPlayer - startGameSession', (group) => {
+  const cache = new CacheService()
+  const eventStream = new EventStreamService()
+  const job = new MatchPlayerJob(cache, eventStream)
+
+  group.teardown(async () => {
+    await cache.flush()
+  })
+
+  test('should start a new game session', async ({ assert }) => {
+    const players = [{ ...session.player1 }, { ...session.player2 }]
+
+    const sessionId = randomUUID()
+
+    const date = '2021-10-10T10:00:00.000Z'
+
+    const newSession = await job.createSession(players[0], players[1], sessionId as GameSessionId)
+
+    const word = await Word.create({ name: 'test', difficulty: 1, language: 'en' })
+    await job.startGameSession(newSession.sessionId, word, players, players[0], players[1])
+
+    // Get the session from cache
+    const cachedSession = JSON.parse(
+      (await cache.get(`game:session:${newSession.sessionId}`)) || ''
+    )
+    assert.isNotNull(cachedSession)
+
+    /**
+     * Update the session to set the players as accepted
+     * + set date, hint giver and the turn to avoid random values in the test
+     */
+    await cache.set(
+      `game:session:${newSession.sessionId}`,
+      JSON.stringify({
+        ...cachedSession,
+        player1: { ...players[0], accepted: true },
+        player2: { ...players[1], accepted: true },
+        hintGiver: players[0].id,
+        turn: players[0].id,
+        startedAt: date,
+      })
+    )
+
+    // Get updated session
+    const result = JSON.parse((await cache.get(`game:session:${newSession.sessionId}`)) || '')
+    assert.isNotNull(result)
+
+    // Check if the session is correct after starting the game
+    assert.deepEqual(result, {
+      sessionId: newSession.sessionId,
+      player1: players[0],
+      player2: players[1],
+      turn: players[0].id,
+      guessed: false,
+      hintGiver: players[0].id,
+      word: word.name,
+      startedAt: date,
+      wordsList: { hintGiver: [], guesser: [] },
+    })
+  })
+})
