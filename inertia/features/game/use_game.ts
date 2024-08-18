6256e9fc42e1f645c08d4053b1104dab21fbe49b
Prisca <prisca.castanier@gmail.com>
2024-08-18 04:17:07 +0200

    feat(hook): add state and onChange hooks to handle word validation state

--- inertia/features/game/use_game.ts
+++ inertia/features/game/use_game.ts
@@ -5,11 +5,20 @@ import type { GameSessionProps } from '~/pages/game_session'
 import { Api } from '~/services/api'
 import type { WordList } from '#features/game_session/types/game_session'
 import type { GameResponseStatus } from '~/features/game/types/game_response_status'
+import {
+  type WordValidationState,
+  WordValidationStateEnum,
+} from '~/features/game/enums/word_validation_state'
 
 type Props = GameSessionProps
+export type WordStateProps = { valid: boolean; status: WordValidationState }
 export const useGame = (props: Props) => {
   const { sessionId, user, wordsList, turn } = props
 
+  const [wordState, setWordState] = useState<WordStateProps>({
+    valid: false,
+    status: WordValidationStateEnum.INVALID,
+  })
   const [hintGiverWords, setHintGiverWords] = useState<string[]>([])
   const [guesserWords, setGuesserWords] = useState<string[]>([])
   const [gameState, setGameState] = useState<GameStateEnum>(GameState.WAITING)
@@ -49,6 +58,29 @@ export const useGame = (props: Props) => {
     form.reset()
   }
 
+  const wordOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const word = event.target.value
+    const haveOnlyLetters = /^[a-zA-Z]+$/.test(word)
+    const haveOnlyOneWord = word.split(' ').length === 1
+
+    if (!word) {
+      setWordState({ valid: false, status: WordValidationStateEnum.NULL })
+      return
+    }
+
+    if (!haveOnlyOneWord) {
+      setWordState({ valid: false, status: WordValidationStateEnum.MANY_WORDS })
+      return
+    }
+
+    if (!haveOnlyLetters) {
+      setWordState({ valid: false, status: WordValidationStateEnum.INVALID_CHARACTERS })
+      return
+    }
+
+    setWordState({ valid: true, status: WordValidationStateEnum.VALID })
+  }
+
   const handleGameState = (message: {
     words: WordList
     status?: GameResponseStatus
@@ -85,5 +117,7 @@ export const useGame = (props: Props) => {
     handleSubmit,
     handleGameState,
     handleCopySessionId,
+    wordState,
+    wordOnChange,
   }
 }
