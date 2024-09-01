c91247dfd04f7027a687fb2f9b33b2fae2814014
Prisca <prisca.castanier@gmail.com>
2024-09-02 00:39:40 +0200

    refactor(home): create a profile card component

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -4,6 +4,7 @@ import { Chat } from '~/features/chat/chat'
 import React from 'react'
 import { Button } from '~/features/utils/components/button'
 import { Container } from '~/features/utils/components/container'
+import { ProfileCard } from '~/features/home/components/profile_card'
 
 export interface HomeProps {
   username: string
@@ -41,34 +42,7 @@ export default function Home(props: HomeProps) {
           </Trans>
         </p>
         <Button onClick={() => router.visit('/game/search')}>{t('home.buttons.start')}</Button>
-        <Container
-          justify={'center'}
-          direction={'col'}
-          gap={4}
-          className={'bg-gray-100 p-4 rounded-md'}
-        >
-          <Container justify={'center'} direction={'col'} gap={4} className={'md:flex-row'}>
-            <img
-              src={avatarUrl}
-              alt={'avatar'}
-              className={'w-20 h-20 rounded-full border-solid border-4 border-gray-200'}
-            />
-            <Container justify={'center'} direction={'col'} gap={2}>
-              <p
-                className={
-                  'text-center bg-gray-200 px-3 py-1rounded-md w-50 rounded-md font-bold truncate'
-                }
-              >
-                {username}
-              </p>
-              <p className={'text-center bg-gray-200 px-3 py-1 rounded-md w-50'}>
-                {t('home.points')}
-                <span className={'block font-bold'}>{elo}</span>
-              </p>
-            </Container>
-          </Container>
-          <Button onClick={() => router.visit('/profile')}>{t('home.buttons.profile')}</Button>
-        </Container>
+        <ProfileCard username={username} avatarUrl={avatarUrl} elo={elo} />
       </Container>
     </Container>
   )
