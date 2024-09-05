cb681347c40914e599421fbcf1905c140553c909
Prisca <prisca.castanier@gmail.com>
2024-09-05 19:34:41 +0200

    fix(profile): fix sorted games not being properly sorted

--- inertia/pages/profile.tsx
+++ inertia/pages/profile.tsx
@@ -5,6 +5,7 @@ import { ProfileCard } from '~/features/home/components/profile_card'
 import { Button } from '~/features/utils/components/button'
 import { router } from '@inertiajs/react'
 import { GamesTable } from '~/features/profile/components/games_table'
+import { DateTime } from 'luxon'
 
 interface ProfileProps {
   user: {
@@ -19,9 +20,14 @@ export default function Profile(props: ProfileProps) {
   const { user, games } = props
   const { t } = useTranslation()
 
-  games.sort((a, b) => {
-    return a.date > b.date ? -1 : 1
-  })
+  const sortedGames = games
+    .sort((a, b) => {
+      return (
+        DateTime.fromFormat(a.date, 'dd/MM/yyyy HH:mm:ss').toMillis() -
+        DateTime.fromFormat(b.date, 'dd/MM/yyyy HH:mm:ss').toMillis()
+      )
+    })
+    .reverse()
 
   return (
     <Container justify={'center'} align={'center'} gap={4} className={'text-center'}>
@@ -29,7 +35,7 @@ export default function Profile(props: ProfileProps) {
       <h1>{t('profile.title')}</h1>
       <ProfileCard username={user.username} avatarUrl={user.avatarUrl!} elo={user.elo} />
       <h2>{t('profile.gameHistory')}</h2>
-      <GamesTable games={games} />
+      <GamesTable games={sortedGames} />
     </Container>
   )
 }
